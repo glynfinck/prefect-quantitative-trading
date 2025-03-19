aecf6a43281f4298c2a8aa71b6c7fde4ec2c445b
Glyn Finck <glynfinck@Glyns-MacBook-Pro.local>
2025-03-19 21:20:55 +0000

    Adding github.

--- projects/markov/markov_deploy.py
+++ projects/markov/markov_deploy.py
@@ -1,12 +1,24 @@
-from prefect import deploy
-from markov import markov
+from prefect import flow
+from prefect_github import GitHubCredentials
+from prefect.runner.storage import GitRepository
 from prefect.docker import DockerImage
 
 if __name__ == "__main__":
-    markov.deploy(
+    source = GitRepository(
+        url="https://github.com/glynfinck/prefect-quantitative-trading.git",
+        credentials=GitHubCredentials.load("github-credentials"),
+        branch="main"
+    )
+    flow.from_source(
+        source=source,
+        entrypoint="projects/markov/markov.py:markov"
+    ).deploy(
         name="markov",
         work_pool_name="default",
-        image="docker.io/glynfinck/prefect-quantitative-trading/markov:latest",
+        image=DockerImage(
+            name="docker.io/glynfinck/markov",
+            tag="latest"
+        ),
         build=False,
         push=False
     )
